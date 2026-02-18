1813f0ca810424d68badcd1c9f2daff3e8eedc63
jayandra7822 <jayandrasaraswat@gmail.com>
2026-02-18 15:44:40 +0530

    first commit

---
+++ nodebackend/index.js
@@ -0,0 +1,46 @@
+const http = require("http");
+const sum = require("./fetchData.js");
+const writeData=require('./usefsmodule.js');
+const PORT = 4007;
+const server = http.createServer( async(req, res) => {
+
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,DELETE,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // res.setHeader('content-type','text/html')
+  // res.end("<h2 style=color:red> Hello Welcome to Node Server </h2>");
+
+  if (req.url == "/msg" && req.method == "GET") {
+    res.setHeader("content-type", "text/html");
+    res.end("<h2 style=color:red> Hello Welcome to Node Server </h2>");
+  }
+
+  if (req.url == "/data" && req.method == "GET") {
+    res.setHeader("content-type", "application/json");
+    const sumData = await sum()
+    res.end(JSON.stringify({ msg: sumData }));
+  }
+  
+  if (req.url == "/writeData" && req.method == "GET") {
+    res.setHeader("content-type", "application/json");
+    const sumData = writeData();
+    res.end(JSON.stringify({ msg: sumData }));
+  }
+
+
+  if (req.url == "/data" && req.method == "POST") {
+    res.setHeader("content-type", "application/json");
+    res.end(JSON.stringify({ msg: "This is JSON POST data" }));
+  }
+
+  if (req.url == "/data" && req.method == "DELETE") {
+    res.setHeader("content-type", "application/json");
+    res.end(JSON.stringify({ msg: "This is JSON delete data" }));
+  }
+  
+});
+
+server.listen(PORT, () => {
+  console.log(`Service is availabe at ${PORT}`);
+});
---
+++ nodebackend/usefsmodule.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const fs1= require('fs').promises
+
+function writeData(){
+    let statusmsg = "";
+    try{
+        fs.writeFileSync("student.txt","welcome to FS Module")
+        console.log("Data is written successfully!!")
+        statusmsg="Data is written successfully"
+    }catch(e){
+        console.log("Error is"+e)
+        statusmsg="Error is"+e;
+    }
+    return statusmsg;
+}
+module.exports=writeData;
